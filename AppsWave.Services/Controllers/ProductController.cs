a2e2df2132e18fb6f5acf1e3604c8c2546fd43d0
Laith <laith.m2002@outlook.com>
2025-11-27 00:27:11 +0300

    Add Product APIs

---
+++ AppsWave.Services/Controllers/ProductController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AppsWave.Services.Controllers
+{
+    [ApiController]
+    [Route("api/product")]
+    [Authorize]
+    public class ProductController : ControllerBase
+    {
+        public IActionResult Index()
+        {
+            return Ok();
+        }
+    }
+}
--- AppsWave.Services/Data/AppDbContext.cs
+++ AppsWave.Services/Data/AppDbContext.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Identity;
+﻿using AppsWave.Entites;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
@@ -14,20 +15,69 @@ namespace AppsWave.Services.Data
         {
             base.OnModelCreating(modelBuilder);
 
-            modelBuilder.Entity<AppsWave.Entites.User>().HasData(new AppsWave.Entites.User
-            { 
-                Email = "MohammadJamal22@gmail.com",
-                FullName = "Mohammad Jamal",
-                UserName = "MJamal",
-            });
+            var adminRoleId = "role-admin";
+            var customerRoleId = "role-customer";
+            var user1Id = "user-1";
+            var user2Id = "user-2";
 
-            modelBuilder.Entity<AppsWave.Entites.User>().HasData(new AppsWave.Entites.User
+            var adminRole = new IdentityRole
             {
-                Email = "TalaLutfi03@gmail.com",
-                FullName = "Tala Lutfi",
-                UserName = "TLutfi",
-            });
+                Id = adminRoleId,
+                Name = "Admin",
+                NormalizedName = "ADMIN"
+            };
 
+            var customerRole = new IdentityRole
+            {
+                Id = customerRoleId,
+                Name = "Customer",
+                NormalizedName = "CUSTOMER"
+            };
+
+            modelBuilder.Entity<IdentityRole>().HasData(adminRole, customerRole);
+
+            var hasher = new PasswordHasher<User>();
+
+            var user1 = new User
+            {
+                Id = user1Id,
+                UserName = "MJamal",
+                NormalizedUserName = "MJAMAL",
+                Email = "MohammadJamal22@gmail.com",
+                NormalizedEmail = "MOHAMMADJAMAL22@GMAIL.COM",
+                EmailConfirmed = true,
+                FullName = "Mohammad Jamal",
+                SecurityStamp = "stamp-1" 
+            };
+            user1.PasswordHash = hasher.HashPassword(user1, "Password123!");
+
+            var user2 = new User
+            {
+                Id = user2Id,
+                UserName = "TLutfi",
+                NormalizedUserName = "TLUTFI",
+                Email = "TalaLutfi03@gmail.com",
+                NormalizedEmail = "TALALUTFI03@GMAIL.COM",
+                EmailConfirmed = true,
+                FullName = "Tala Lutfi",
+                SecurityStamp = "stamp-2"
+            };
+            user2.PasswordHash = hasher.HashPassword(user2, "Password123!");
+
+            modelBuilder.Entity<User>().HasData(user1, user2);
+
+            modelBuilder.Entity<IdentityUserRole<string>>().HasData(
+                new IdentityUserRole<string>
+                {
+                    RoleId = adminRoleId,
+                    UserId = user1Id
+                },
+                new IdentityUserRole<string>
+                {
+                    RoleId = customerRoleId,
+                    UserId = user2Id
+                }
+            );
         }
-    }
+        }
 }
